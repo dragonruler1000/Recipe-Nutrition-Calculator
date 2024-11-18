0055107b6adfc246b2889a85c80e4ba93fc9430c
dragonruler1000 <zach_github@minec1.us>
2024-11-18 14:27:00 -0600

    Create MainActivity.kt
    Adding initial functionality

---
+++ app/src/main java/com/recipeanalyzer/recipeanalyzer/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.yourappname.recipeanalyzer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.yourappname.recipeanalyzer.navigation.AppNavHost
+import com.yourappname.recipeanalyzer.ui.theme.RecipeAnalyzerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RecipeAnalyzerTheme {
+                AppNavHost()
+            }
+        }
+    }
+}
